ecc4df3f714cc8a6d1a00e4acc54d8957da4f52d
seonghyunMin <steve6133@naver.com>
2024-03-16 16:00:41 +0900

    feat : redis incr 적용

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,10 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
+    private final CouponCountRepository couponCountRepository;
+
     public void apply(Long userId){
-        long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
 
         if(count > 100){
             return;
